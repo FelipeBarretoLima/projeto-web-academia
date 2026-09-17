67353abff2613a638ea13c61c1957c2f7e9ea507
Felipe <felipebarretolima10@gmail.com>
2026-09-17 18:04:24 -0300

    Configuração inicial do projeto Spring Boot

---
+++ RegistroTreinos/src/main/java/br/uel/RegistroTreinos/RegistroTreinosApplication.java
@@ -0,0 +1,13 @@
+package br.uel.RegistroTreinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroTreinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroTreinosApplication.class, args);
+	}
+
+}
