5b46059afaecae7ed797b30d4dd7da795c1b36a8
Felipe <felipebarretolima10@gmail.com>
2026-09-22 18:32:06 -0300

    ADD verificacao de usuario, data e nome_treino igual, impede a inserção de treinos duplicados

--- RegistroTreinos/src/main/java/br/uel/RegistroTreinos/Repository/TreinoRepository.java
+++ RegistroTreinos/src/main/java/br/uel/RegistroTreinos/Repository/TreinoRepository.java
@@ -1,10 +1,12 @@
 package br.uel.RegistroTreinos.Repository;
 
 import br.uel.RegistroTreinos.Model.Treino;
+import br.uel.RegistroTreinos.Model.Usuario;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -12,4 +14,7 @@ public interface TreinoRepository extends JpaRepository<Treino, Long> {
 
     // Busca treinos cujo usuário tem o nome contendo o texto (ignora maiúsculas/minúsculas)
     List<Treino> findByUsuarioNomeContainingIgnoreCase(String nome, Sort sort);
+
+    // Verifica se já existe um treino com esse usuário, data e nome de treino
+    boolean existsByUsuarioAndDateAndNomeTreino(Usuario usuario, LocalDate date, String nomeTreino);
 }
